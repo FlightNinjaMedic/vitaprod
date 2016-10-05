06b44792fe295d7d2763c13f74e30b784ec39fc9
Hector Lozano <hlozano2@cnm.edu>
2016-10-05 17:45:07 -0600

    Added test index.php file

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>The Vitamin Store</title>
+		<link rel="stylesheet" type="text/css" href="simple.css"/>
+	</head>
+	<body>
+		<header>
+			<h1 class="Centered">Devil Doc Vitamin Store</h1>
+			<p>Are You Suffering From PTSD?</p>
+			<p>Try Our New Vitamin Hemp Concentrate. On Special for $10/Unit</p>
+		</header>
+		<table summary="Home Page">
+			<tr>
+				<td width="20%"><a href="index.html">Home</a> </td>
+				<td width="20%"><a href="buy.html">Buy Now</a> </td>
+				<td width="20%"><a href="products.html">Products and Services</a> </td>
+				<td width="20%"><a href="yourhealth.html"></a> </td>
+				<td width="20%"><a href="about.html">About Us</a> </td>
+			</tr>
+			<tr>
+				<td colspan="3">
+					<h1>Welcome to the Vitamin Store: Protecting Your Health Naturally!</h1>
+					<p>Founded April 30, 2016 The Vitamin Store is veteran owned and operated.
+						We offer the best herbal medication that money can buy</p>
+					<p>Many Companies that talk about Customer Service fail to deliver.
+						We've Served Our Country, Now We Are Redy To Serve You!</p>
+					<fieldset>
+						<legend><h3>Customer Information</h3></legend>
+						<table summary="Customer Information">
+							<tr>
+								<td>Salutation:</td>
+								<td><select name="Salute">
+										<option> </option>
+										<option>Mr.</option>
+										<option>Mrs.</option>
+										<option>Ms.</option>
+									</select> </td>
+							</tr>
+
+							<tr>
+								<td><label for="First Name">First Name:</label></td>
+								<td><input id="First Name" type="text" name="First Name" size="40" required /></td>
+								<td><label for="Last Name">Last Name:</label> </td>
+								<td><input id="Last Name" type="text" name="Last Name" size="40" required /></td>
+							</tr>
+
+							<tr>
+								<td><label for="Address">Address:</label></td>
+								<td><input id="Address" type="text" name="Address" size="40" required /></td>
+								<td><label for="City">City:</label></td>
+								<td><input id="City" type="text" name="City" size="40" required /></td>
+							</tr>
+
+							<tr>
+								<td><label for="State">State:</label> </td>
+								<td><input id="State" type="text" name="State" size="40" required /></td>
+								<td><label for="Zip Code">Zip Code:</label> </td>
+								<td><input id="Zip Code" type="text" name="Zip Code" size="40" required/></td>
+							</tr>
+						</table>
+					</fieldset>
+		</table>
+		<footer>
+			<h1 align="center">Follow Us On Facebook And Twitter</h1>
+		</footer>
+	</body>
+</html>
